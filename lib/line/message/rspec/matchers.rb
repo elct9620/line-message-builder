f5ca64a7ab9f86dfe3b55c8a0d60acbf1bd4cb78
Aotokitsuruya <contact@aotoki.me>
2025-04-05 21:07:25 +0800

    refactor: Consolidate flex message matchers into HaveFlexComponent class

--- lib/line/message/rspec/matchers.rb
+++ lib/line/message/rspec/matchers.rb
@@ -2,4 +2,6 @@
 
 require_relative "matchers/have_text_message"
 require_relative "matchers/have_quick_reply"
+
 require_relative "matchers/have_flex_message"
+require_relative "matchers/have_flex_component"
---
+++ lib/line/message/rspec/matchers/have_flex_component.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+module Line
+  module Message
+    module RSpec
+      # :nodoc:
+      module Matchers
+        # The flex component matcher for RSpec to search nested flex components in the message array.
+        class HaveFlexComponent
+          def initialize(expected_desc: nil, &expected)
+            @expected = expected
+            @expected_desc = expected_desc
+          end
+
+          def description
+            return "have flex component" if @expected.nil?
+            return "have flex component matching #{@expected_desc}" if @expected_desc
+
+            "have flex component matching #{@expected.inspect}"
+          end
+
+          def matches?(actual)
+            @actual = actual
+            @actual.any? { |message| match_flex_component?(message) }
+          end
+
+          def failure_message
+            return "expected to find a flex component" if @expected.nil?
+            return "expected to find a flex component matching #{@expected_desc}" if @expected_desc
+
+            "expected to find a flex component matching #{@expected.inspect}"
+          end
+
+          private
+
+          def match_flex_component?(message)
+            return false unless message[:type] == "flex"
+
+            message[:contents].any? { |content| match_content?(content) }
+          end
+
+          def match_content?(content)
+            return match_bubble?(content) if content[:type] == "bubble"
+            return content[:contents].any? { |nested_content| match_content?(nested_content) } if content[:contents]
+
+            @expected.call(content)
+          end
+
+          def match_bubble?(content)
+            %i[header hero body footer].each do |key|
+              next unless content[key]
+
+              return true if match_content?(content[key])
+            end
+
+            false
+          end
+        end
+
+        def have_line_flex_component(&) # rubocop:disable Naming/PredicateName
+          HaveFlexComponent.new(&)
+        end
+
+        def have_line_flex_text(text) # rubocop:disable Naming/PredicateName
+          HaveFlexComponent.new(expected_desc: "text(#{text.inspect})") do |content|
+            content[:type] == "text" && content[:text].match?(text)
+          end
+        end
+
+        def have_line_flex_button(type, **args) # rubocop:disable Naming/PredicateName
+          HaveFlexComponent.new(expected_desc: "#{type} button(#{args.inspect})") do |content|
+            next false unless content[:type] == "button"
+
+            ::RSpec::Matchers::BuiltIn::Include.new({ type: type, **args }).matches?(content[:action])
+          end
+        end
+
+        def have_line_flex_image(url, **args) # rubocop:disable Naming/PredicateName
+          HaveFlexComponent.new(expected_desc: "image(#{url.inspect})") do |content|
+            next false unless content[:type] == "image"
+
+            ::RSpec::Matchers::BuiltIn::Include.new({ url: url, **args }).matches?(content)
+          end
+        end
+      end
+    end
+  end
+end
